a51d547a4f041809bb58c274bff8f42f3dcdd093
unknown <yasmin.rodriguez@gmail.com>
2020-12-31 08:21:26 -0500

    initial project setup

---
+++ src/main/java/com/makolyte/springsnsstarter/SpringSnsStarterApplication.java
@@ -0,0 +1,13 @@
+package com.makolyte.springsnsstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSnsStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSnsStarterApplication.class, args);
+	}
+
+}
