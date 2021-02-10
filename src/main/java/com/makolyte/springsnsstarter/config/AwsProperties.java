29dfa4778245115a04c8275e6d268458472a239f
unknown <yasmin.rodriguez@gmail.com>
2021-02-10 08:14:38 -0500

    add actuator and validation dependencies

--- src/main/java/com/makolyte/springsnsstarter/config/AwsProperties.java
+++ src/main/java/com/makolyte/springsnsstarter/config/AwsProperties.java
@@ -3,12 +3,16 @@ package com.makolyte.springsnsstarter.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import javax.validation.constraints.NotNull;
+
 @Configuration
 @ConfigurationProperties(prefix = "aws.sns")
 public class AwsProperties {
 
+    @NotNull
     private String region;
 
+    @NotNull
     private String topicArn;
 
     public String getRegion() {
